fa70fb4e937442118a8c55dcf95bfdad26c987ca
ericsun <eric.sun@spgs-shengbo.com>
2025-03-04 17:04:40 +0800

    Added a button that changes the background picture

--- gui.py
+++ gui.py
@@ -3,7 +3,8 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 
-
+def hi():
+    label_pic.config(image=bannerABackground)
 
 #gui window
 root = tk.Tk()
@@ -11,11 +12,15 @@ root.title("Honkai Star Rail")
 root.geometry("500x400+50+50")
 
 #Background images
-image = tk.PhotoImage(file='backgroundImage/starrail.png')
-tk.Label(root, image=image).pack()
+background = tk.PhotoImage(file='backgroundImage/starrail.png')
+bannerABackground = tk.PhotoImage(file='backgroundImage/standard banner.png')
+label_pic = tk.Label(root, image=background)
+label_pic.pack(pady=15)
 
 #side buttons for switching banners
-#characterBannerA = ttk.Button(root, text='Character Banner A', command= )
+characterBannerA = ttk.Button(root, text='Character Banner A', command=hi)
+characterBannerA.pack(side=tk.LEFT)
+
 
 
 
