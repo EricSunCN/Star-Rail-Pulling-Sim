b559a7153ab031d9635101c6bf64e095aaaf585f
ericsun <eric.sun@spgs-shengbo.com>
2025-03-05 19:24:28 +0800

    added resizable images and resized some of them. Also added pillow

--- gui.py
+++ gui.py
@@ -1,38 +1,57 @@
 #imports
 import tkinter as tk
-from idlelib.debugger_r import restart_subprocess_debugger
 from tkinter import ttk
-from pull import *
 from tkinter import *
+from PIL import Image, ImageTk
+
+from pull import *
 
 #Updates the background image
-def change_bg():
-    bg_pic.config(image=bannerABackground)
+def changeBgStandard():
+    bg_pic.config(image=standardBannerBgFinal)
+
+def changeBgLimitedOne():
+    bg_pic.config(image=limitedBannerBgFinal)
+    pull.pack()
 
 #gui window
 root = tk.Tk()
 root.title("Honkai Star Rail")
-root.geometry("750x600+50+50")
+root.geometry("700x700+50+50")
 
 #Background images
-default = tk.PhotoImage(file='backgroundImage/starrail.png')
-bannerABackground = tk.PhotoImage(file='backgroundImage/standard banner.png')
+default = Image.open('backgroundImage/defaultBackground.jpg')
+standardBannerBg = Image.open('backgroundImage/standardBannerBackground.jpeg')
+limitedBannerBg = Image.open('backgroundImage/limitedBannerBackground.jpg')
+
+#Resize
+defaultResized = default.resize((700,400))
+defaultFinal = ImageTk.PhotoImage(defaultResized)
+standardBannerBgResized = standardBannerBg.resize((700,400))
+standardBannerBgFinal = ImageTk.PhotoImage(standardBannerBgResized)
+limitedBannerBgResized = limitedBannerBg.resize((700,400))
+limitedBannerBgFinal = ImageTk.PhotoImage(limitedBannerBgResized)
 
 #Background_Label
-bg_pic = tk.Label(root, image=default)
+bg_pic = Label(root, image=defaultFinal)
+bg_pic.image = default
 bg_pic.pack(pady=15)
 
 #side buttons for switching banners
-standardBanner = ttk.Button(root, text='Standard Banner', command=change_bg)
+standardBanner = ttk.Button(root, text='Standard Banner', command=changeBgStandard)
 standardBanner.pack(side=tk.LEFT)
 
+#
+limitedBannerOne = ttk.Button(root, text='Limited Banner', command=changeBgLimitedOne)
+limitedBannerOne.pack(side=tk.LEFT)
 
 pull = ttk.Button(root, text='10 Pull', command=tenPull)
 pull.pack(side=tk.LEFT)
 
-item = ttk.Label(root, text=tenPullResults)
+item = Label(root, text=tenPullResults)
 item.pack(side=tk.BOTTOM)
 
+pull.pack_forget()
 
 
 root.mainloop()
