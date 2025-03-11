cec80070e8577e712ba81d91bbc0cb6961e168aa
eranstraub <erans1@icloud.com>
2025-03-11 16:21:17 +0800

    Add assets folder, as more than just background images will exist. Rewrote gui to fix some 🐞. Made main.py the actual entrypoint, instead of gui....

--- gui.py
+++ gui.py
@@ -1,121 +1,100 @@
-#imports
+import os
 import tkinter as tk
 from tkinter import ttk
-from tkinter import *
 from PIL import Image, ImageTk
-from pull import *
+from pull import singlePull, tenPull, limitedFiveStarOne, limitedFiveStarTwo
 
+# Global Variables
 allowPullDisplay = False
+root = None
+bg_pic = None
+singlePullButton = None
+tenPullButton = None
+item = None
 
-#Updates the background image to standard banner
 def changeBgStandard():
     bg_pic.config(image=standardBannerBgFinal)
-#Changes the background image to the limited banner
+
 def changeBgLimitedOne():
     global allowPullDisplay
     bg_pic.config(image=limitedBannerOneBgFinal)
     limitedFiveStarOne()
-    if allowPullDisplay == False:
-        tenPullButton.pack()
-        singlePullButton.pack()
-        singlePullButton.place(x=395, y=475)
-        tenPullButton.place(x=550, y=475)
-        allowPullDisplay = True
-    print(allowPullDisplay)
+    showPullButtons()
+
 def changeBgLimitedTwo():
     global allowPullDisplay
     bg_pic.config(image=limitedBannerTwoBgFinal)
     limitedFiveStarTwo()
-    if allowPullDisplay == False:
-        tenPullButton.pack()
-        singlePullButton.pack()
+    showPullButtons()
+
+def showPullButtons():
+    global allowPullDisplay
+    if not allowPullDisplay:
         singlePullButton.place(x=395, y=475)
         tenPullButton.place(x=550, y=475)
         allowPullDisplay = True
-    print(allowPullDisplay)
-    #inglePullButton.place(x=395, y=475)
-    #tenPullButton.place(x=550, y=475)
+
 def tenPullFunction():
-    tenPull()
-    item.config(text=tenPullResults)
+    results = tenPull()
 
+def start_gui():
+    global root, bg_pic, singlePullButton, tenPullButton, item
+    global standardBannerBgFinal, limitedBannerOneBgFinal, limitedBannerTwoBgFinal
+    global standardBannerIcFinal, limitedBannerOneIcFinal, limitedBannerTwoIcFinal
+    global singlePullIcFinal, tenPullIcFinal
 
-#gui window
-root = tk.Tk()
-root.title("Honkai Star Rail")
-root.geometry("700x700+50+50")
+    root = tk.Tk()
+    root.title("Honkai Star Rail")
+    root.geometry("700x700+50+50")
 
-#results
-item = Label(root, text='')
-item.pack(side=tk.BOTTOM)
+    # Results Label
+    item = tk.Label(root, text='', font=("Arial", 12))
+    item.pack(side=tk.BOTTOM)
 
+    # Get the absolute path of the assets directory
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    ASSETS_PATH = os.path.join(BASE_DIR, "assets", "backgroundImage")
 
-#Background images
-default = Image.open('backgroundImage/defaultBackground.jpg')
-standardBannerBg = Image.open('backgroundImage/standardBannerBackground.jpeg')
-standardBannerIc = Image.open('backgroundImage/standardBannerIcon.jpg')
-limitedBannerOneBg = Image.open('backgroundImage/limitedBannerOneBackground.jpg')
-limitedBannerOneIc = Image.open('backgroundImage/limitedBannerOneIcon.jpg')
-limitedBannerTwoBg = Image.open('backgroundImage/limitedBannerTwoBackground.jpg')
-limitedBannerTwoIc = Image.open('backgroundImage/limitedBannerTwoIcon.jpg')
-singlePullIc = Image.open('backgroundImage/singlePull.jpg')
-tenPullIc = Image.open('backgroundImage/tenPull.jpg')
-#Add all of this to json file
+    # Load and Resize Images
+    def load_image(filename, size):
+        return ImageTk.PhotoImage(Image.open(os.path.join(ASSETS_PATH, filename)).resize(size))
 
-#Resize
-defaultResized = default.resize((700,400))
-defaultFinal = ImageTk.PhotoImage(defaultResized)
-standardBannerBgResized = standardBannerBg.resize((700,450))
-standardBannerBgFinal = ImageTk.PhotoImage(standardBannerBgResized)
-limitedBannerOneBgResized = limitedBannerOneBg.resize((700,450))
-limitedBannerOneBgFinal = ImageTk.PhotoImage(limitedBannerOneBgResized)
-limitedBannerTwoBgResized = limitedBannerTwoBg.resize((700,450))
-limitedBannerTwoBgFinal = ImageTk.PhotoImage(limitedBannerTwoBgResized)
+    defaultFinal = load_image('defaultBackground.jpg', (700, 400))
+    standardBannerBgFinal = load_image('standardBannerBackground.jpeg', (700, 450))
+    limitedBannerOneBgFinal = load_image('limitedBannerOneBackground.jpg', (700, 450))
+    limitedBannerTwoBgFinal = load_image('limitedBannerTwoBackground.jpg', (700, 450))
 
-standardBannerIcResized = standardBannerIc.resize((100,50))
-standardBannerIcFinal = ImageTk.PhotoImage(standardBannerIcResized)
-limitedBannerOneIcResized = limitedBannerOneIc.resize((100,50))
-limitedBannerOneIcFinal = ImageTk.PhotoImage(limitedBannerOneIcResized)
-limitedBannerTwoIcResized = limitedBannerTwoIc.resize((100,50))
-limitedBannerTwoIcFinal = ImageTk.PhotoImage(limitedBannerTwoIcResized)
-singlePullIcResized = singlePullIc.resize((125,30))
-singlePullIcFinal = ImageTk.PhotoImage(singlePullIcResized)
-tenPullIcResized = tenPullIc.resize((125,30))
-tenPullIcFinal = ImageTk.PhotoImage(tenPullIcResized)
-#Background_Label
-bg_pic = Label(root, image=defaultFinal)
-bg_pic.image = default
-bg_pic.pack(pady=15)
+    standardBannerIcFinal = load_image('standardBannerIcon.jpg', (100, 50))
+    limitedBannerOneIcFinal = load_image('limitedBannerOneIcon.jpg', (100, 50))
+    limitedBannerTwoIcFinal = load_image('limitedBannerTwoIcon.jpg', (100, 50))
+    singlePullIcFinal = load_image('singlePull.jpg', (125, 30))
+    tenPullIcFinal = load_image('tenPull.jpg', (125, 30))
 
-#side buttons for switching banners
-standardBanner = tk.Button(root, text='Standard Banner', width=100, height=50, command=changeBgStandard, image=standardBannerIcFinal)
-standardBanner.pack(side=tk.LEFT)
-limitedBannerOne = tk.Button(root, text='Limited Banner A', width=100, height=50, command=changeBgLimitedOne, image=limitedBannerOneIcFinal)
-limitedBannerOne.pack(side=tk.LEFT)
-limitedBannerTwo = tk.Button(root, text='Limited Banner B', width=100, height=50, command=changeBgLimitedTwo, image=limitedBannerTwoIcFinal)
-limitedBannerTwo.pack()
-#Places the banner buttons in the correct space
-limitedBannerOne.place(x=0,y=475)
-limitedBannerTwo.place(x=0,y=530)
-standardBanner.place(x=0,y=585)
-#Pulling buttons
-singlePullButton = tk.Button(root, text='1 Pull', width=140, height=30, command=singlePull, image=singlePullIcFinal)
-singlePullButton.pack(side=tk.LEFT)
-tenPullButton = tk.Button(root, text='10 Pull', width=140, height=30, command=tenPullFunction, image=tenPullIcFinal)
-tenPullButton.pack(side=tk.BOTTOM)
+    # Background Label
+    bg_pic = tk.Label(root, image=defaultFinal)
+    bg_pic.image = defaultFinal
+    bg_pic.pack(pady=15)
 
-#Exist button
-exitButton = ttk.Button(root, text='Exit', command=exit)
-exitButton.pack(side=tk.RIGHT)
+    # Banner Buttons
+    standardBanner = tk.Button(root, image=standardBannerIcFinal, command=changeBgStandard, borderwidth=0)
+    limitedBannerOne = tk.Button(root, image=limitedBannerOneIcFinal, command=changeBgLimitedOne, borderwidth=0)
+    limitedBannerTwo = tk.Button(root, image=limitedBannerTwoIcFinal, command=changeBgLimitedTwo, borderwidth=0)
 
-#Results
+    # Positioning
+    standardBanner.place(x=0, y=585)
+    limitedBannerOne.place(x=0, y=475)
+    limitedBannerTwo.place(x=0, y=530)
 
+    # Pulling Buttons
+    singlePullButton = tk.Button(root, image=singlePullIcFinal, command=singlePull, borderwidth=0)
+    tenPullButton = tk.Button(root, image=tenPullIcFinal, command=tenPullFunction, borderwidth=0)
 
-#Hides the pull buttons on the home screen
-tenPullButton.pack_forget()
-singlePullButton.pack_forget()
+    # Exit Button
+    exitButton = ttk.Button(root, text='Exit', command=root.quit)
+    exitButton.pack(side=tk.RIGHT)
 
+    # Hide pull buttons on the home screen
+    singlePullButton.place_forget()
+    tenPullButton.place_forget()
 
-
-root.mainloop()
-
+    root.mainloop()
